dab0978bd7b16fdddbaf1734cd39b814a12cb8c3
David Himmelstrup <lemmih@gmail.com>
2021-07-19 20:38:34 +0800

    Use uint up to 4k bits.

--- benches/shootout.rs
+++ benches/shootout.rs
@@ -2,14 +2,17 @@ use criterion::{
     criterion_group,
     criterion_main,
     measurement::WallTime,
-    Bencher,
+    BatchSize,
+    // Throughput,
+    // Bencher,
     BenchmarkGroup,
     Criterion,
-    // Throughput,
 };
 
 use num_bigint::{BigInt, RandBigInt};
 use num_integer::Integer;
+
+use uint::construct_uint;
 
 #[cfg(feature = "rug")]
 use rug::Integer as RugInteger;
@@ -18,18 +21,43 @@ use rug::Integer as RugInteger;
 use ramp::int::Int as RampInt;
 
 mod rng;
-use rand::RngCore;
+// use rand::RngCore;
 use rng::get_rng;
+
+construct_uint! {
+    pub struct U1024(16);
+}
+
+// uint doesn't go higher than this.
+construct_uint! {
+    pub struct U4096(64);
+}
+
+macro_rules! uint {
+    ( $group: ident, $bits: ident, $ty:ty, $run: expr ) => {
+        $group.bench_function("uint", |b| {
+            let mut rng = get_rng();
+            // let x = <$ty>::from_str_radix(&rng.gen_biguint($bits).to_str_radix(16), 16).unwrap();
+            // let y = <$ty>::from_str_radix(&rng.gen_biguint($bits).to_str_radix(16), 16).unwrap();
+            b.iter_batched(
+                || {
+                    let x = <$ty>::from_str_radix(&rng.gen_biguint($bits).to_str_radix(16), 16)
+                        .unwrap();
+                    let y = <$ty>::from_str_radix(&rng.gen_biguint($bits).to_str_radix(16), 16)
+                        .unwrap();
+                    (x, y)
+                },
+                |(x, y)| ($run)(x, y),
+                BatchSize::SmallInput,
+            )
+        });
+    };
+}
 
 type BenchGroup = fn(_: &mut BenchmarkGroup<'_, WallTime>, bits: u64);
 
-fn mk_benchmark(c: &mut Criterion, name: &str, run_group: BenchGroup, native: fn(b: &mut Bencher)) {
-    let mut group = c.benchmark_group(format!("{}/64bits", name));
-    group.bench_function("native", native);
-    run_group(&mut group, 64);
-    group.finish();
-
-    for &bits in [128, 1024, 4096, 32768].iter() {
+fn mk_benchmark(c: &mut Criterion, name: &str, run_group: BenchGroup) {
+    for &bits in [64, 128, 1024, 4096, 32768].iter() {
         let mut group = c.benchmark_group(format!("{}/{}bits", name, bits));
         run_group(&mut group, bits);
         group.finish();
@@ -53,10 +81,14 @@ fn bigint(
 ) {
     group.bench_function("num", |b| {
         let mut rng = get_rng();
-        let x = rng.gen_bigint(bits);
-        let y = rng.gen_bigint(bits);
+        // let x = rng.gen_bigint(bits);
+        // let y = rng.gen_bigint(bits);
 
-        b.iter(|| run(&x, &y))
+        b.iter_batched(
+            || (rng.gen_bigint(bits), rng.gen_bigint(bits)),
+            |(x, y)| run(&x, &y),
+            BatchSize::SmallInput,
+        )
     });
 }
 
@@ -68,9 +100,18 @@ fn rug(
 ) {
     group.bench_function("rug", |b| {
         let mut rng = get_rng();
-        let x = bigint_to_rug(rng.gen_bigint(bits));
-        let y = bigint_to_rug(rng.gen_bigint(bits));
-        b.iter(|| run(&x, &y))
+        // let x = bigint_to_rug(rng.gen_bigint(bits));
+        // let y = bigint_to_rug(rng.gen_bigint(bits));
+        b.iter_batched(
+            || {
+                (
+                    bigint_to_rug(rng.gen_bigint(bits)),
+                    bigint_to_rug(rng.gen_bigint(bits)),
+                )
+            },
+            |(x, y)| run(&x, &y),
+            BatchSize::SmallInput,
+        )
     });
 }
 
@@ -82,45 +123,19 @@ fn ramp(
 ) {
     group.bench_function("ramp", |b| {
         let mut rng = get_rng();
-        let x = bigint_to_ramp(rng.gen_bigint(bits));
-        let y = bigint_to_ramp(rng.gen_bigint(bits));
-        b.iter(|| run(&x, &y))
+        // let x = bigint_to_ramp(rng.gen_bigint(bits));
+        // let y = bigint_to_ramp(rng.gen_bigint(bits));
+        b.iter_batched(
+            || {
+                (
+                    bigint_to_ramp(rng.gen_bigint(bits)),
+                    bigint_to_ramp(rng.gen_bigint(bits)),
+                )
+            },
+            |(x, y)| run(&x, &y),
+            BatchSize::SmallInput,
+        )
     });
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Multiply benches
-
-fn mul_bench_native(b: &mut Bencher) {
-    let mut rng = get_rng();
-    b.iter(|| rng.next_u64() * rng.next_u64());
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Division benches
-
-fn div_bench_native(b: &mut Bencher) {
-    let mut rng = get_rng();
-    b.iter(|| rng.next_u64() / rng.next_u64());
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Addition benches
-
-fn add_bench_native(b: &mut Bencher) {
-    let mut rng = get_rng();
-    b.iter(|| rng.next_u64() + rng.next_u64());
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// GCD benches
-
-fn gcd_bench_native(b: &mut Bencher) {
-    let mut rng = get_rng();
-    let x = rng.next_u64();
-    let y = rng.next_u64();
-
-    b.iter(|| x.gcd(&y));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -135,14 +150,20 @@ fn gcd_group(group: &mut BenchmarkGroup<'_, WallTime>, bits: u64) {
 }
 
 fn mul_group(group: &mut BenchmarkGroup<'_, WallTime>, bits: u64) {
-    // if bits == 1024 {
-    //     group.bench_function("uint", |b| {
-    //         let mut rng = get_rng();
-    //         let x = U1024::from_str_radix(&rng.gen_biguint(bits).to_str_radix(16), 16).unwrap();
-    //         let y = U1024::from_str_radix(&rng.gen_biguint(bits).to_str_radix(16), 16).unwrap();
-    //         b.iter(|| x.overflowing_mul(y))
-    //     });
-    // }
+    if bits == 64 {
+        uint!(group, bits, u64, |x: u64, y: u64| x.overflowing_mul(y));
+    }
+    if bits == 128 {
+        uint!(group, bits, u128, |x: u128, y: u128| x.overflowing_mul(y));
+    }
+    if bits == 1024 {
+        uint!(group, bits, U1024, |x: U1024, y: U1024| x
+            .overflowing_mul(y));
+    }
+    if bits == 4096 {
+        uint!(group, bits, U4096, |x: U4096, y: U4096| x
+            .overflowing_mul(y));
+    }
     bigint(group, bits, |x, y| x * y);
     #[cfg(feature = "rug")]
     rug(group, bits, |x, y| RugInteger::from(x * y));
@@ -151,6 +172,20 @@ fn mul_group(group: &mut BenchmarkGroup<'_, WallTime>, bits: u64) {
 }
 
 fn add_group(group: &mut BenchmarkGroup<'_, WallTime>, bits: u64) {
+    if bits == 64 {
+        uint!(group, bits, u64, |x: u64, y: u64| x.overflowing_add(y));
+    }
+    if bits == 128 {
+        uint!(group, bits, u128, |x: u128, y: u128| x.overflowing_add(y));
+    }
+    if bits == 1024 {
+        uint!(group, bits, U1024, |x: U1024, y: U1024| x
+            .overflowing_add(y));
+    }
+    if bits == 4096 {
+        uint!(group, bits, U4096, |x: U4096, y: U4096| x
+            .overflowing_add(y));
+    }
     bigint(group, bits, |x, y| x + y);
     #[cfg(feature = "rug")]
     rug(group, bits, |x, y| RugInteger::from(x + y));
@@ -159,6 +194,18 @@ fn add_group(group: &mut BenchmarkGroup<'_, WallTime>, bits: u64) {
 }
 
 fn div_group(group: &mut BenchmarkGroup<'_, WallTime>, bits: u64) {
+    if bits == 64 {
+        uint!(group, bits, u64, |x: u64, y: u64| x.checked_div(y));
+    }
+    if bits == 128 {
+        uint!(group, bits, u128, |x: u128, y: u128| x.checked_div(y));
+    }
+    if bits == 1024 {
+        uint!(group, bits, U1024, |x: U1024, y: U1024| x.checked_div(y));
+    }
+    if bits == 4096 {
+        uint!(group, bits, U4096, |x: U4096, y: U4096| x.checked_div(y));
+    }
     bigint(group, bits, |x, y| x / y);
     #[cfg(feature = "rug")]
     rug(group, bits, |x, y| RugInteger::from(x / y));
@@ -169,27 +216,12 @@ fn div_group(group: &mut BenchmarkGroup<'_, WallTime>, bits: u64) {
 ///////////////////////////////////////////////////////////////////////////////
 // Benchmarks
 
-fn gcd_benchmark(c: &mut Criterion) {
-    mk_benchmark(c, "gcd", gcd_group, gcd_bench_native);
+fn benchmarks(c: &mut Criterion) {
+    mk_benchmark(c, "gcd", gcd_group);
+    mk_benchmark(c, "mul", mul_group);
+    mk_benchmark(c, "div", div_group);
+    mk_benchmark(c, "add", add_group);
 }
 
-fn mul_benchmark(c: &mut Criterion) {
-    mk_benchmark(c, "mul", mul_group, mul_bench_native);
-}
-
-fn div_benchmark(c: &mut Criterion) {
-    mk_benchmark(c, "div", div_group, div_bench_native);
-}
-
-fn add_benchmark(c: &mut Criterion) {
-    mk_benchmark(c, "add", add_group, add_bench_native);
-}
-
-criterion_group!(
-    benches,
-    gcd_benchmark,
-    mul_benchmark,
-    div_benchmark,
-    add_benchmark
-);
+criterion_group!(benches, benchmarks,);
 criterion_main!(benches);
